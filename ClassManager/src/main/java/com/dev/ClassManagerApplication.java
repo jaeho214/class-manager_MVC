5d682655021d56431084904ff5d90c9c0bb847ec
jaeho214 <hyeonpig@gmail.com>
2019-05-23 00:45:44 +0900

    My Batis 연동

--- ClassManager/src/main/java/com/dev/ClassManagerApplication.java
+++ ClassManager/src/main/java/com/dev/ClassManagerApplication.java
@@ -1,13 +1,14 @@
 package com.dev;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@MapperScan("com.dev.mapper")
 public class ClassManagerApplication {
 
 	public static void main(String[] args) {
